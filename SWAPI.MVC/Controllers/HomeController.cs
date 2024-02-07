2e11c3a869a41e608e25319242d85407d73a0a93
Terry Brown <31456269+jhondoe256@users.noreply.github.com>
2024-02-06 20:07:40 -0500

    Started People Controller

--- SWAPI.MVC/Controllers/HomeController.cs
+++ SWAPI.MVC/Controllers/HomeController.cs
@@ -6,16 +6,30 @@ namespace SWAPI.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<string> _names;
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            //                               0       1      2        3
+            _names = new List<string>() { "Terry","Rigo","Benard","Jessi"};
         }
 
         public IActionResult Index()
         {
-            return View();
+            //get a random numeric value....
+            //we want to make sure that we consider the _names collections .Count()
+            //4ppl max
+            //remember? Collections start at zero!!!
+            Random rnd = new Random();
+            //this will give back an index value that we can use to "grab" a specific name
+            int value = rnd.Next(0, _names.Count);
+
+            //TempData["name"] = _names[value];
+
+            return View(model: _names[value]);
         }
 
         public IActionResult Privacy()
@@ -28,5 +42,7 @@ namespace SWAPI.MVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
---
+++ SWAPI.MVC/Controllers/PeopleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWAPI.MVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+
+        public PeopleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = httpClientFactory.CreateClient("swapi");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
